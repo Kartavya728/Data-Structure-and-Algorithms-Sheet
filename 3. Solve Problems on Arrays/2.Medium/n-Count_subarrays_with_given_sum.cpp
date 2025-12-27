f46a843e5bb2575f60fbe5d4f57782d8222f6ab1
Kartavya728 <kartavya.xenon@gmail.com>
2025-12-27 18:53:42 +0530

    completed medium level array problems in the sheet

--- 3. Solve Problems on Arrays/2.Medium/n-Count_subarrays_with_given_sum.cpp
+++ 3. Solve Problems on Arrays/2.Medium/n-Count_subarrays_with_given_sum.cpp
@@ -1,7 +1,24 @@
 // Count subarrays with given sum
-
+//link-https://leetcode.com/problems/subarray-sum-equals-k/
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        for(int i=1;i<n;i++)nums[i]+=nums[i-1];
+        unordered_map<int,int> um;
+        int sum,cnt=0;
+        for(int i=0;i<n;i++){
+            sum=nums[i];
+            if(sum==k)cnt++;
+            if(um.find(sum-k)!=um.end())cnt+=um[sum-k];
+            um[sum]+=1;
+        }
+        return cnt;
+    }
+};
 
 int main(){
     
